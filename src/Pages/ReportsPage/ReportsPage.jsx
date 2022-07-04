570549c61690e3882ea6edf7f3e31376c8ffd858
BIT Student <classroom@bgit.rs>
2022-07-04 14:10:49 +0200

    create-header-footer-reportpage

--- src/Pages/ReportsPage/ReportsPage.jsx
+++ src/Pages/ReportsPage/ReportsPage.jsx
@@ -1,8 +1,106 @@
 import React from "react";
-import "./reportsPage.scss"
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import "./reportsPage.scss";
 
-function ReportsPage (props){
-    return <div>Reports Page</div>
+function ReportsPage(props) {
+  return (
+    <div>
+      <Header />
+      <div className="report">
+        <div className="reports-component">
+          <input type="Search" placeholder="Search" />
+          <div className="reports-list">
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+
+            <div className="info">
+              <h2>Company</h2>
+              <h2>Candidate</h2>
+              <h2>Interview Date</h2>
+              <h2>Status</h2>
+              <h3>(@)</h3>
+              <h3>X</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default ReportsPage
+export default ReportsPage;
