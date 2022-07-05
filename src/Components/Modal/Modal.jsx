2605ffd7fa493e9e44b280fc2cae2ab0679102ad
BIT Student <classroom@bgit.rs>
2022-07-05 15:21:53 +0200

    pocela modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./modal.scss";
 
 function Modal (props) {
-    return <div id="modal">Modal</div>
+    return <div id="modal">
+        <div className="wrapper">
+            
+        </div>
+    </div>
 }
 
 export default Modal;
